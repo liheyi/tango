f47dac11a348a9d12e9b471b628566570a4b5db4
spadesuit <spadesuit007@gmail.com>
2017-12-25 18:35:27 +0800

    How to Tango with Django 1.9 in 2017-12-25

---
+++ rango/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first paramater is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html', '')
